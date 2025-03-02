65c2a08bd571ef3c88dce68aa51794b8252fd7ed
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2025-03-02 17:34:57 -0400

    feat: remove app file and add integration with graphql

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,32 @@
-import { app } from "./app";
+import "reflect-metadata"
+import fastify from "fastify"
+import { appDataSource } from "./typeorm/data-source"
+import dbConn from "typeorm-fastify-plugin"
+import { ApolloServer } from "@apollo/server"
+import { buildGraphQLSchema } from "./buildGraphQLSchema"
+import fastifyApollo, { fastifyApolloDrainPlugin } from "@as-integrations/fastify";
 
-app.listen({
-	host: '0.0.0.0',
-  port: 3333
-}).then(() => console.log('Server runnning'))
+export const server = async () => {
+  const PORT = 3333
+  const app = fastify()
+
+  app.register(dbConn, {
+    connection: appDataSource
+  })
+
+  const schema = await buildGraphQLSchema()
+
+  const apolloServer = new ApolloServer({ schema, plugins: [fastifyApolloDrainPlugin(app)], });
+  await apolloServer.start();
+
+  await app.register(fastifyApollo(apolloServer));
+
+  app.get('/hello', (req, res) => res.send('Hello world'))
+
+  app.listen({
+    host: '0.0.0.0',
+    port: PORT
+  }).then(() => console.log(`🚀 Server runnning http://localhost/${PORT}\nGraphQL http://localhost/${PORT}`))
+}
+
+server()
