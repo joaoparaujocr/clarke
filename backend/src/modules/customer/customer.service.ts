a7fcdbd346dc86c95b299c4526775dccdcf31fd9
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2025-03-03 17:35:46 -0400

    feat: create service customer

---
+++ backend/src/modules/customer/customer.service.ts
@@ -0,0 +1,78 @@
+import Customer from '@typeorm/entities/customer';
+import { inject, injectable } from 'tsyringe'
+import { CustomerRepository } from './customer.repository';
+import { CreateCustomerInput } from './dtos/createCustomerInput.dto';
+import { compare, hash } from 'bcryptjs';
+import { AppError } from '../../error/appError';
+import { AuthCustomerInput } from './dtos/authCustomerInput.dto';
+import { FastifyReply } from 'fastify';
+import { Message } from './dtos/message.dto';
+
+@injectable()
+export class CustomerService {
+  constructor(@inject(CustomerRepository) private readonly customerRepository: CustomerRepository) { }
+
+  async getAll(): Promise<Customer[]> {
+    return await this.customerRepository.findAll()
+  }
+
+  async create(data: CreateCustomerInput) {
+    const alreadyExistsCustomer = await this.customerRepository.findByEmail(data.email)
+
+    if (alreadyExistsCustomer) {
+      throw new AppError(409, 'Email already exists')
+    }
+
+    const passwordHash = await hash(data.password, 12)
+
+    return await this.customerRepository.create({ ...data, password: passwordHash })
+  }
+
+  async auth(data: AuthCustomerInput, reply: FastifyReply) {
+    const customer = await this.customerRepository.findByEmail(data.email)
+
+    if (!customer) {
+      throw new AppError(401, 'Invalid credentials')
+    }
+
+    const doesPasswordMatch = await compare(data.password, customer.password)
+
+    if (!doesPasswordMatch) {
+      throw new AppError(401, 'Invalid credentials')
+    }
+
+    const { email, id } = customer
+
+    const payload = { email, id, type: "CUSTOMER" }
+
+    const token = await reply.jwtSign(payload, {
+      sign: {
+        sub: id,
+      }
+    })
+
+    const refreshToken = await reply.jwtSign(payload, {
+      sign: {
+        sub: id,
+        expiresIn: '7d'
+      }
+    })
+
+    reply.setCookie('refreshToken', refreshToken, { httpOnly: true, sameSite: true, secure: true, path: '/' })
+
+    return {
+      token,
+      customer
+    }
+  }
+
+  async me(id: string) {
+    return await this.customerRepository.findById(id)
+  }
+
+  logout(reply: FastifyReply): Message {
+    reply.clearCookie('refreshToken', { path: '/', httpOnly: true, sameSite: true, secure: true });
+
+    return { message: 'Logout successful' }
+  }
+}
