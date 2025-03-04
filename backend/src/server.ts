0937c4227402f66350a261524a8acf9a4f1836eb
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2025-03-04 17:20:19 -0400

    feat: add cors setting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import { buildGraphQLSchema } from "./buildGraphQLSchema"
 import fastifyApollo, { fastifyApolloDrainPlugin } from "@as-integrations/fastify";
 import fastifyJwt from "@fastify/jwt"
 import fastifyCookie from "@fastify/cookie"
+import cors from '@fastify/cors'
 import { handleError } from "./error/handleError"
 import { ContextType } from "./context/context.dto"
 import { contextApolloServer } from "./context"
@@ -15,6 +16,14 @@ import { contextApolloServer } from "./context"
 export const server = async () => {
   const PORT = 3333
   const app = fastify()
+
+  app.register(cors, {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    credentials: true,
+    hook: 'preHandler',
+  });
 
   app.register(dbConn, {
     connection: appDataSource
